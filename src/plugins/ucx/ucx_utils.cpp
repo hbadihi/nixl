bb30566ef10fef90d3a80f4a2d8f21a2e0108725
Tomer Davidor <tdavidor@nvidia.com>
2026-04-28 20:05:51 +0300

    ucx_proxy_backend: Add UCX atomic add support
    Signed-off-by: Tomer Davidor <tdavidor@nvidia.com>

--- src/plugins/ucx/ucx_utils.cpp
+++ src/plugins/ucx/ucx_utils.cpp
@@ -320,6 +320,31 @@ nixlUcxEp::write(void *laddr,
 }
 
 nixl_status_t
+nixlUcxEp::atomicAdd(uint64_t value,
+                     uint64_t raddr,
+                     const nixl::ucx::rkey &rkey,
+                     nixlUcxReq &req) {
+    nixl_status_t status = checkTxState();
+    if (status != NIXL_SUCCESS) {
+        return status;
+    }
+
+    ucp_request_param_t param = {
+        .op_attr_mask = UCP_OP_ATTR_FIELD_DATATYPE,
+        .datatype = ucp_dt_make_contig(sizeof(uint64_t)),
+    };
+
+    const ucs_status_ptr_t request =
+        ucp_atomic_op_nbx(eph, UCP_ATOMIC_OP_ADD, &value, 1, raddr, rkey.get(), &param);
+    if (UCS_PTR_IS_PTR(request)) {
+        req = static_cast<nixlUcxReq>(request);
+        return NIXL_IN_PROG;
+    }
+
+    return nixl::ucx::ucsToNixlStatus(UCS_PTR_STATUS(request));
+}
+
+nixl_status_t
 nixlUcxEp::estimateCost(size_t size,
                         std::chrono::microseconds &duration,
                         std::chrono::microseconds &err_margin,
